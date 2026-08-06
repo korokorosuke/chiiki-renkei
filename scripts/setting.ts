ccebf7fcd3c98fd1c1695fcb3c5122a8c0c99323
korokorosuke <134833411+korokorosuke@users.noreply.github.com>
2026-08-06 11:39:41 +0900

    feat: DenoKVでファイル名を設定できるように変更

--- scripts/setting.ts
+++ scripts/setting.ts
@@ -1,1 +1,6 @@
-export const KV_PATH = "";
+import { DB_URL } from "../src/server/settings.ts"
+let path = Deno.env.get(DB_URL);
+if(!path){
+  path = "reco.db";
+}
+export const KV_PATH = path;
--- src/server/infra/kv.ts
+++ src/server/infra/kv.ts
@@ -1,6 +1,6 @@
 /// <reference lib="deno.unstable" />
 import type { Database } from "./database.ts"
-import { PRODUCTION_TYPE, DEV } from "../settings.ts"
+import { PRODUCTION_TYPE, DEV, DB_URL } from "../settings.ts"
 
 
 export class Kv implements Database{
@@ -13,17 +13,26 @@ export class Kv implements Database{
             return this.openTest();
         }
 
+        const path = Deno.env.get(DB_URL)
         const dev = Deno.env.get(PRODUCTION_TYPE)
         if(dev === DEV){
-          return this.openDev();
+          return this.openDev(path);
         }
 
-        this.kv = await Deno.openKv();
+        if(path && path !== ""){
+          this.kv = await Deno.openKv(path);
+        }else{
+          this.kv = await Deno.openKv();
+        }
         return this.kv;
     }
 
-    async openDev(): Promise<Deno.Kv> {
-      this.kv = await Deno.openKv("reco.db");
+    async openDev(path: string|undefined): Promise<Deno.Kv> {
+      if(path){
+        this.kv = await Deno.openKv("zdev_" + path);
+      }else{
+        this.kv = await Deno.openKv("zdev_reco.db");
+      }
       return this.kv;
     }
 
